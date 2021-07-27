4ad0ad1ad57b1daad2e2dd3a5cbc709d83f3d96c
Da-Mell <Damellfaines8@gmail.com>
2021-07-27 18:36:47 -0400

    update

--- BigO /bigO.js
+++ BigO /bigO.js
@@ -1,9 +1,14 @@
+// const addUpTo = n => {
+//     let total = 0;
+//     for (let i = 0; i <= n; ++i){
+//         total += i;
+//     }
+//     return total;
+// }
+
 const addUpTo = n => {
-    let total = 0;
-    for (let i = 0; i <= n; ++i){
-        total += i;
-    }
-    return total;
+    return n * (n + 1) / 2;
 }
 
 console.log(addUpTo(9))
+console.log(addUpTo(89))
---
+++ BigO /tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const addUpTo = n => {
+//     let total = 0;
+//     for (let i = 0; i <= n; ++i){
+//         total += i;
+//     }
+//     return total;
+// }
